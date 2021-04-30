0b3fc66247b243e7240780ca0a474a3a0746e2a9
joaobnd <joao.martins@ccc.ufcg.edu.br>
2021-04-29 21:22:21 -0300

    alterando configuração da porta

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const app = require('./app')
-const port = 3003
+const port = process.env.PORT || 3003;
 mongoose.connect('mongodb+srv://develop:senha152@cluster0.udoxk.mongodb.net/backend-todolist?retryWrites=true&w=majority',  { useNewUrlParser: true,
 useUnifiedTopology: true,
 useCreateIndex: true}).then(() => {
